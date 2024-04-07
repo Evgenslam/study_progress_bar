37818f51a9c2ef06ffb5102ecb89c91c1555d483
EvgenSlam <evgenyb86@gmail.com>
2024-04-07 18:49:15 +0700

    Add create and update time columnds in models

--- progress_bar_project/progress_bar_app/models.py
+++ progress_bar_project/progress_bar_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Textbook(models.Model):
     name = models.CharField(max_length=100)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -12,6 +14,8 @@ class Lesson(models.Model):
     textbook = models.ForeignKey(Textbook, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
