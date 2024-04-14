389e6db104b6cb627a86b8c9492510ebdb2d25c4
EvgenSlam <evgenyb86@gmail.com>
2024-04-14 14:42:15 +0700

    Alter extract_bookmarks utility. Get lesson_data

--- progress_bar_project/bookmarks_extractor.py
+++ progress_bar_project/bookmarks_extractor.py
@@ -1,27 +1,35 @@
 import csv
 import fitz  # PyMuPDF
-
-from variables import textbooks_data
-
-
-def extract_bookmarks(pdf_path, csv_path, textbook):
-    bookmarks = []
-    # Open the PDF file
-    pdf_document = fitz.open(pdf_path)
-
-    # Iterate through bookmarks
-    for bookmark in pdf_document.get_toc():
-        print(bookmark)
-        name = bookmark[1]
-        bookmarks.append({'Textbook': textbook, 'Name': name})
-
-    # Write bookmarks to CSV
-    with open(csv_path, 'w', newline='', encoding='utf-8') as csv_file:
-        fieldnames = ['Textbook', 'Name']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(bookmarks)
+import os
 
 
-for t in textbooks_data:
-    extract_bookmarks(t["pdf_path"], t["csv_path"], t["textbook"])
+def extract_bookmarks(pdf_folder):
+    root, _, files = next(os.walk(pdf_folder))
+
+    for file in files:
+        textbook = file.rstrip('.pdf')
+        pdf_path = os.path.join(root, file)
+        csv_path = os.path.join('lesson_data/', f'{textbook}.csv')
+
+        bookmarks = []
+        # Open the PDF file
+        pdf_document = fitz.open(pdf_path)
+
+        # Iterate through bookmarks
+        for bookmark in pdf_document.get_toc():
+            print(bookmark)
+            name = bookmark[1]
+            bookmarks.append({'Textbook': textbook, 'Name': name})
+
+        # Create lesson_data folder if doesn't exist
+        os.makedirs('lesson_data', exist_ok=True)
+
+        # Write bookmarks to CSV
+        with open(csv_path, 'w', newline='', encoding='utf-8') as csv_file:
+            fieldnames = ['Textbook', 'Name']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(bookmarks)
+
+
+extract_bookmarks('pdf_files/')
